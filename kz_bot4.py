ca03e047e8db8e83c59fa2b38a635a6999077bc0
Cooler-tu <rebecca_siyao@outlook.com>
2025-11-16 00:18:45 +0800

    Update

--- kz_bot4.py
+++ kz_bot4.py
@@ -236,9 +236,6 @@ class DynamicMomentumBot:
                     self.client.place_order(sym, side, amount)
                     logger.info(f"→ {side.upper()} {abs(amount):.6f} {sym} (${abs(diff_usd):,.0f})")
 
-                new_balance= ExchangeClient.get_balance(self)
-                logger.info(f"购买后现金余额为{new_balance}")
-
         except Exception as e:
             logger.error(f"step 错误: {e}", exc_info=True)
 
